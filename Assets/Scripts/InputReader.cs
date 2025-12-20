93e41ca60ecf2fcd2f5d57195cf5f6ae21e6c310
efebastug <bastugefe@hotmail.com>
2025-12-20 16:57:31 +0300

    Deneme 2

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -4,7 +4,7 @@
  [CreateAssetMenu(fileName = "InputReader", menuName = "Input/Input Reader")]
  public class InputReader : ScriptableObject, Controls.IPlayerActions
  {
-    //Sa
+    //Sashka made me do it
     private Controls _controls;
     public event Action<Vector2> MoveEvent;
     public event Action<bool> PrimaryFireEvent;
