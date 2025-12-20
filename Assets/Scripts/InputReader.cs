360fa4ab1add176d0d9cc12e46ff7785d8940d88
efebastug <bastugefe@hotmail.com>
2025-12-20 16:20:00 +0300

    Sa

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -4,6 +4,7 @@
  [CreateAssetMenu(fileName = "InputReader", menuName = "Input/Input Reader")]
  public class InputReader : ScriptableObject, Controls.IPlayerActions
  {
+    //Sa
     private Controls _controls;
     public event Action<Vector2> MoveEvent;
     public event Action<bool> PrimaryFireEvent;
